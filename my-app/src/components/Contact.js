41c93de611bacf3ef7af918dbb03478ef8420ac5
Darsan Qi <darsan.qitian@gmail.com>
2024-05-08 10:53:57 -0600

    added components

---
+++ my-app/src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <section id="contact">
+      <h2>Contact</h2>
+      <p>If you would like to get in touch, please email me at <a href="mailto:example@example.com">example@example.com</a>.</p>
+    </section>
+  );
+}
+
+export default Contact;
