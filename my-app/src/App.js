d1dafa113a049d04f33ba8fb2ea314301da64fd9
Darsan Qi <darsan.qitian@gmail.com>
2024-05-08 11:10:55 -0600

    basic structure complete

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Portfolio from './components/Portfolio';
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Portfolio />
+      <About />
+      <Contact />
+      <Footer />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
