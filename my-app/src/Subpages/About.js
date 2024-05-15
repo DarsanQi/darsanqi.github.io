38dd7a36acbabf32d6b5b1bbfe63c190bdcd7915
Darsan Qi <78439241+DarsanQi@users.noreply.github.com>
2024-05-15 16:22:37 +0000

    home and about page done

--- my-app/src/Subpages/About.js
+++ my-app/src/Subpages/About.js
@@ -1,27 +1,44 @@
+import React, { useState, useEffect } from 'react';
 
 const About = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <section id="about">
-      <div className="about_background_image"></div>
-      <div className="about-content">
-        <div className="about-greeting">
-          <h1>Hi there!</h1>
-          <h1>I'm</h1>
-          <h1>Darsan.</h1>
+    <div className={isMobile ? "mobile-container" : "container"}>
+      <section id="about">
+        <div className="about-content">
+          <div className="about-greeting">
+            <h1>Hi there!</h1>
+            <h1>I'm</h1>
+            <h1>Darsan.</h1>
+          </div>
+          <div className="about-description">
+            <p>
+              I am currently an aspiring computer engineer at the University of Toronto. I'm super passionate about embedded systems programming, electronics, and software development.
+            </p>
+            <p>
+              I like to spend my time working cool personal projects to hone the skills and abilities that allow me to make a lasting and meaningful impact in the real world.
+            </p>
+            <p>
+              Please feel free to take a look at some of my projects! I hope you find them as interesting as I do.
+            </p>
+          </div>
         </div>
-        <div className="about-description">
-          <p>
-            I am currently an aspiring computer engineer at the University of Toronto. I'm super passionate about embedded systems programming, electronics, and software development.
-          </p>
-          <p>
-             I like to spend my time working cool personal projects to hone the skills and abilities that allow me to make a lasting and meaningful impact in the real world.
-          </p>
-          <p>
-            Please feel free to take a look at some of my projects! I hope you find them as interesting as I do.
-          </p>
-        </div>
-      </div>
-    </section>
+      </section>
+      {!isMobile && <div className="about-background-image"></div>}
+      {isMobile && <div className="mobile-background-image"></div>}
+    </div>
   );
 }
 
