d60a177553221b835f8273a800c1010478bd768f
Darsan Qi <78439241+DarsanQi@users.noreply.github.com>
2024-05-15 17:01:14 +0000

    rough version of projects page created

--- my-app/src/Subpages/Projects.js
+++ my-app/src/Subpages/Projects.js
@@ -1,25 +1,40 @@
+const projects = [
+  {
+    title: "Project 1",
+    image: "path/to/image1.jpg",
+    description: "This is a short description of project 1."
+  },
+  {
+    title: "Project 2",
+    image: "path/to/image2.jpg",
+    description: "This is a short description of project 2."
+  },
+  {
+    title: "Project 3",
+    image: "path/to/image3.jpg",
+    description: "This is a short description of project 3."
+  },
+  {
+    title: "Project 4",
+    image: "path/to/image4.jpg",
+    description: "This is a short description of project 4."
+  }
+];
 
 const Projects = () => {
   return (
     <section id="projects">
-      <div className="project-content">
-        <div className="project-title">
-          <h1>Projects</h1>
-        </div>
-        <div className="project-name">
-          <p>
-            FPGA Human Benchmark
-          </p>
-          <p>
-            Custom ground up CPU architecture
-          </p>
-          <p>
-            Rocket Flight path simulator - NIOS Orbit
-          </p>
-          <p>
-            Arduino Car
-          </p>
-        </div>
+      <h1>Projects</h1>
+      <div className="projects-container">
+        {projects.map((project, index) => (
+          <div className="project-card" key={index}>
+            <img src={project.image} alt={project.title} className="project-image" />
+            <div className="project-details">
+              <h2 className="project-title">{project.title}</h2>
+              <p className="project-description">{project.description}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </section>
   );
