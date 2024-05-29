0e34ada63af4dccb5b46efb8f06a03d2445b103a
Darsan Qi <78439241+DarsanQi@users.noreply.github.com>
2024-05-29 16:15:33 +0000

    contact page work, looks presentable

--- my-app/src/Subpages/Contact.js
+++ my-app/src/Subpages/Contact.js
@@ -3,11 +3,8 @@ const Contact = () => {
   return (
     <section id="contact" className="contact-section">
       <div className="contact-container">
-        <div className="contact-image">
-          <img src="path_to_your_image.png" alt="Contact Illustration" />
-        </div>
         <div className="contact-form-container">
-          <h2>Looks like you need some help!!!</h2>
+          <h2>Looks like you need some help</h2>
           <h1>Reach Out!</h1>
           <p>
             I usually respond within an hour, but designers do sleep two to three hours a night, so it might take a bit longer.
@@ -19,24 +16,27 @@ const Contact = () => {
             <textarea placeholder="Message" name="message" required></textarea>
             <div className="form-buttons">
               <button type="submit">Submit</button>
-              <button type="button" className="schedule-meeting-button">Schedule A Meeting</button>
             </div>
           </form>
+          <contact-buttons>
+            <a href="https://www.linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-linkedin"></i>
+            </a>
+            <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-github"></i>
+            </a>
+            <a href="mailto:your-email@example.com">
+              <i className="fas fa-envelope"></i>
+            </a>
+            <a href="tel:+1234567890">
+              <i className="fas fa-phone"></i>
+            </a>
+          </contact-buttons>
+          
         </div>
-      </div>
-      <div className="contact-icons-contact">
-        <a href="https://www.linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer">
-          <i className="fab fa-linkedin"></i>
-        </a>
-        <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer">
-          <i className="fab fa-github"></i>
-        </a>
-        <a href="mailto:your-email@example.com">
-          <i className="fas fa-envelope"></i>
-        </a>
-        <a href="tel:+1234567890">
-          <i className="fas fa-phone"></i>
-        </a>
+        <div className="contact-image">
+          <img src="/assets/background_images/contact_line.jpg" alt="Contact Illustration" />
+        </div>
       </div>
     </section>
   );
