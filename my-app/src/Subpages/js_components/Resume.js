8e1b339b84cf196acbf3a249dbdd160d01284792
Darsan Qi <78439241+DarsanQi@users.noreply.github.com>
2024-09-17 03:13:31 +0000

    added a timeline for the resume page

--- my-app/src/Subpages/js_components/Resume.js
+++ my-app/src/Subpages/js_components/Resume.js
@@ -1,91 +1,13 @@
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+
 const Resume = () => {
   return (
-    <section id="resume">
-      <div className="resume-container">
-        <h2>Experience</h2>
-        <div className="resume-section">
-          <h3>Advanced Analytics Intern</h3>
-          <p>Pembina Pipeline | Calgary, Canada</p>
-          <p>May 2023 - September 2023</p>
-          <ul>
-            <li>Contributed to the development of the ‘Peak Power Avoidance’ project, systematically reducing pipeline flow rates during high surge pricing by validating over 100,000 rows of data.</li>
-            <li>Utilized data visualization tools and developed machine learning models (Linear Regression, Polynomial Regression, Ridge, and Lasso) to forecast flow rates, resulting in annual cost reductions of over $800,000.</li>
-            <li>Automated manual tasks using Python and SQL scripts on Databricks, saving a projected 20 hours of company time monthly.</li>
-          </ul>
-        </div>
-        <div className="resume-section">
-          <h3>Business Applications Intern</h3>
-          <p>Pembina Pipeline | Calgary, Canada</p>
-          <p>May 2024 - September 2024</p>
-          <ul>
-            <li>Validated facility transport data through a machine learning-driven data validation tool, improving data accuracy to approximately 97% and saving an estimated $150,000 annually in labor costs.</li>
-            <li>Enhanced security by developing a Python-based automation tool to manage termination tickets, processing 200-300 tickets per month and saving 40 hours of work time monthly.</li>
-            <li>Migrated local production code to a cloud-based virtual machine and assisted in developing Azure DevOps pipelines for improved scalability and maintenance.</li>
-          </ul>
-        </div>
-        <div className="resume-section">
-          <h3>Engineering Communications Liaison</h3>
-          <p>ESP II | Toronto, Canada</p>
-          <p>January 2023 - May 2023</p>
-          <ul>
-            <li>Fostered internal communication within a team of 5 by developing professional documents and schedules.</li>
-            <li>Played a key role in client and stakeholder communications, ensuring requirements were met through proactive engagement.</li>
-            <li>Innovated a knee brace support solution that was 70% cheaper than average market prices, enhancing functionality over existing solutions.</li>
-          </ul>
-        </div>
-        <div className="resume-section">
-          <h3>Hardware Lead</h3>
-          <p>University of Toronto Creative Computing Association | Toronto, Canada</p>
-          <p>June 2024 - Present</p>
-          <ul>
-            <li>Led firmware development and embedded systems design using the ESP32 microcontroller, completing the hardware controller for Project Alpha.</li>
-            <li>Coordinated project management efforts, resulting in the timely and successful completion of Project Alpha.</li>
-          </ul>
-        </div>
-        <div className="resume-section">
-          <h3>FPGA Human Benchmark</h3>
-          <p>University of Toronto | Toronto, Canada</p>
-          <p>October 2023 - December 2023</p>
-          <ul>
-            <li>Drafted block diagrams, ran logic simulations, and used Verilog to code various IO devices on the Altera De1-SoC FPGA chip, creating interactive skill-based tests inspired by Human Benchmark games.</li>
-            <li>Developed modules to display user scores on the FPGA chip in real time, interfacing with PS/2 mouse and keyboard protocols.</li>
-          </ul>
-        </div>
-        <div className="resume-section">
-          <h3>Nios Orbit - Rocket Flight Path Simulator</h3>
-          <p>University of Toronto | Toronto, Canada</p>
-          <p>March 2024 - April 2024</p>
-          <ul>
-            <li>Developed a rocket flight path simulator on the Altera De1-SoC board using C and Assembly, accurately visualizing the rocket trajectory based on various parameters.</li>
-            <li>Designed and implemented comprehensive state logic, ensuring seamless transitions between simulation stages.</li>
-          </ul>
-        </div>
-        <div className="resume-section">
-          <h3>8-bit Central Processing Unit</h3>
-          <p>Personal Project</p>
-          <p>May 2024 - Present</p>
-          <ul>
-            <li>Designed and built a fully functional 8-bit CPU from scratch using 7400 series chips on breadboards, featuring an arithmetic logic unit, memory block, register file, and custom-encoded operation codes.</li>
-            <li>Designed digital circuits using truth tables, block diagrams, and schematics, validating functionality through Logisim simulations.</li>
-          </ul>
-        </div>
-        <div className="resume-section">
-          <h3>ESP32 Remote Controlled Vehicle</h3>
-          <p>Personal Project</p>
-          <p>May 2024 - Present</p>
-          <ul>
-            <li>Engineered a remote-controlled vehicle using the ESP32 microcontroller, featuring a custom-designed chassis created with Fusion 360.</li>
-            <li>Developed a control interface using an Xbox One controller and a custom Python script, controlling the vehicle via WiFi and integrating a camera system for live video feedback.</li>
-          </ul>
-        </div>
-
-        <h2>Skills</h2>
-        <div className="resume-section">
-          <p>Python, C, C++, Java, Assembly, HTML, Javascript, CSS, FPGA, SQL, Verilog, JSON, Excel VBA, MATLAB, Palantir Foundry, Databricks, Azure DevOps, Logisim, Firmware Development, Embedded Systems Design, hardware assembly, hardware debugging, digital circuit design, Fusion 360, Arduino, ESP32</p>
-        </div>
-
+    <>
+      {/* Centered Education Section */}
+      <div style={{ textAlign: 'center', marginBottom: '30px' }}>
         <h2>Education</h2>
-        <div className="resume-section">
+        <div style={{ display: 'inline-block', textAlign: 'left' }}>
           <h3>University of Toronto</h3>
           <p>Bachelor of Applied Science in Computer/Electrical Engineering with a Minor in Mechatronics | PEY Co-op</p>
           <p>Expected April 2027</p>
@@ -95,7 +17,142 @@ const Resume = () => {
           </ul>
         </div>
       </div>
-    </section>
+
+      {/* Vertical Timeline for other sections */}
+      <VerticalTimeline>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+          date="2022 - present"
+        >
+          <h3 className="vertical-timeline-element-title">Education</h3>
+          <h4 className="vertical-timeline-element-subtitle">University of Toronto</h4>
+          <p>
+            Bachelor of Applied Science in Computer/Electrical Engineering with a Minor in Mechatronics | PEY Co-op<br/> Expected April 2027<br/> GPA: 3.0/4.0
+          </p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+          date="May 2023 - September 2023"
+        >
+          <h3 className="vertical-timeline-element-title">Advanced Analytics Intern</h3>
+          <h4 className="vertical-timeline-element-subtitle">Pembina Pipeline | Calgary, Canada</h4>
+          <p>
+            Contributed to the development of the ‘Peak Power Avoidance’ project, systematically reducing pipeline flow rates during high surge pricing by validating over 100,000 rows of data.<br/>
+            Utilized data visualization tools and developed machine learning models (Linear Regression, Polynomial Regression, Ridge, and Lasso) to forecast flow rates, resulting in annual cost reductions of over $800,000.<br/>
+            Automated manual tasks using Python and SQL scripts on Databricks, saving a projected 20 hours of company time monthly.
+          </p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+          date="May 2024 - September 2024"
+        >
+          <h3 className="vertical-timeline-element-title">Business Applications Intern</h3>
+          <h4 className="vertical-timeline-element-subtitle">Pembina Pipeline | Calgary, Canada</h4>
+          <p>
+            Validated facility transport data through a machine learning-driven data validation tool, improving data accuracy to approximately 97% and saving an estimated $150,000 annually in labor costs.<br/>
+            Enhanced security by developing a Python-based automation tool to manage termination tickets, processing 200-300 tickets per month and saving 40 hours of work time monthly.<br/>
+            Migrated local production code to a cloud-based virtual machine and assisted in developing Azure DevOps pipelines for improved scalability and maintenance.
+          </p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+          date="January 2023 - May 2023"
+        >
+          <h3 className="vertical-timeline-element-title">Engineering Communications Liaison</h3>
+          <h4 className="vertical-timeline-element-subtitle">ESP II | Toronto, Canada</h4>
+          <p>
+            Fostered internal communication within a team of 5 by developing professional documents and schedules.<br/>
+            Played a key role in client and stakeholder communications, ensuring requirements were met through proactive engagement.<br/>
+            Innovated a knee brace support solution that was 70% cheaper than average market prices, enhancing functionality over existing solutions.
+          </p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+          date="June 2024 - Present"
+        >
+          <h3 className="vertical-timeline-element-title">Hardware Lead</h3>
+          <h4 className="vertical-timeline-element-subtitle">University of Toronto Creative Computing Association | Toronto, Canada</h4>
+          <p>
+            Led firmware development and embedded systems design using the ESP32 microcontroller, completing the hardware controller for Project Alpha.<br/>
+            Coordinated project management efforts, resulting in the timely and successful completion of Project Alpha.
+          </p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+          date="October 2023 - December 2023"
+        >
+          <h3 className="vertical-timeline-element-title">FPGA Human Benchmark</h3>
+          <h4 className="vertical-timeline-element-subtitle">University of Toronto | Toronto, Canada</h4>
+          <p>
+            Drafted block diagrams, ran logic simulations, and used Verilog to code various IO devices on the Altera De1-SoC FPGA chip, creating interactive skill-based tests inspired by Human Benchmark games.<br/>
+            Developed modules to display user scores on the FPGA chip in real time, interfacing with PS/2 mouse and keyboard protocols.
+          </p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+          date="March 2024 - April 2024"
+        >
+          <h3 className="vertical-timeline-element-title">Nios Orbit - Rocket Flight Path Simulator</h3>
+          <h4 className="vertical-timeline-element-subtitle">University of Toronto | Toronto, Canada</h4>
+          <p>
+            Developed a rocket flight path simulator on the Altera De1-SoC board using C and Assembly, accurately visualizing the rocket trajectory based on various parameters.<br/>
+            Designed and implemented comprehensive state logic, ensuring seamless transitions between simulation stages.
+          </p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+          date="May 2024 - Present"
+        >
+          <h3 className="vertical-timeline-element-title">8-bit Central Processing Unit</h3>
+          <h4 className="vertical-timeline-element-subtitle">Personal Project</h4>
+          <p>
+            Designed and built a fully functional 8-bit CPU from scratch using 7400 series chips on breadboards, featuring an arithmetic logic unit, memory block, register file, and custom-encoded operation codes.<br/>
+            Designed digital circuits using truth tables, block diagrams, and schematics, validating functionality through Logisim simulations.
+          </p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+          date="May 2024 - Present"
+        >
+          <h3 className="vertical-timeline-element-title">ESP32 Remote Controlled Vehicle</h3>
+          <h4 className="vertical-timeline-element-subtitle">Personal Project</h4>
+          <p>
+            Engineered a remote-controlled vehicle using the ESP32 microcontroller, featuring a custom-designed chassis created with Fusion 360.<br/>
+            Developed a control interface using an Xbox One controller and a custom Python script, controlling the vehicle via WiFi and integrating a camera system for live video feedback.
+          </p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          // iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
+        />
+      </VerticalTimeline>
+    </>
   );
 }
 
